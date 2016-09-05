ef9896ae58a3620d0aae678ad79693b3e214dab5
zengdamo <zengdamo@163.com>
2016-09-05 19:28:44 +0800

    lesson2

---
+++ demo/9.1课堂笔记.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zengyichao on 16/9/1.
+ */
+
+var obj = {
+    name:'zengyichao'
+    walk: function(x){
+        console.log('走', x, '分钟.')
+    }
+}
+console.log(obj.name);
+obj.walk(5);
--- demo/script.js
+++ demo/script.js
@@ -8,3 +8,28 @@ function myfunction(){
     }
     document.getElementById('demo').innerHTML = x;
 }
+
+//9.1课堂笔记
+
+//var obj = {
+//    name:'zengyichao'
+//    walk: function(x){
+//        console.log('走', x, '分钟.')
+//    }
+//}
+//console.log(obj.name);
+//obj.walk(5);
+
+var arr = [0];
+for (i=1;i<100;i++){
+    arr.push(i)
+}
+document.write(arr);
+
+//课堂练习
+
+var str = 'hello, world';
+var arr = [1,2,3,4];
+var arr2 = [7,8,9];
+arr.concat(arr2);
+
